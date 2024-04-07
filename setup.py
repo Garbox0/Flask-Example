22d445af48773fa62dbd3ac62b0733152e160d78
Gabriel <gbrlescalada@gmail.com>
2024-04-07 12:15:26 -0300

    v7

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Flask-Example',
+    version='0.1',
+    description='A simple Flask application',
+    author='Likkl',
+    packages=['flask_example'],
+    install_requires=['Flask'],
+)
